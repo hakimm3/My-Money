221d0e9d50a7eaec0e37a25d44858c2a0151ea28
Trisa Abdul Hakim <hakimpbg@gmail.com>
2023-03-01 07:08:10 +0700

    change locale to jakarta

--- database/seeders/PengeluaranSeeder.php
+++ database/seeders/PengeluaranSeeder.php
@@ -18,7 +18,7 @@ class PengeluaranSeeder extends Seeder
         $user = \App\Models\User::get();
         $categories = \App\Models\Category::get();
 
-        for ($i = 0; $i < 100; $i++) {
+        for ($i = 0; $i < 10000; $i++) {
             \App\Models\Pengeluaran::create([
                 'user_id' => $user->random()->id,
                 'category_id' => $categories->random()->id,
