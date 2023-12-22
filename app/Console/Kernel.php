0ae864b97cf5eaa24d0192fea74d4c802a8033f4
Trisa Abdul Hakim <hakimpbg@gmail.com>
2023-12-22 14:28:52 +0700

    check cronjob heroku

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -53,20 +53,24 @@ class Kernel extends ConsoleKernel
         $title = "Backup Data";
 
         $users = \App\Models\User::all();
-        foreach($users as $user){
-            if(!is_dir(storage_path('app/public/backup/'. $user->email))){
-                mkdir(storage_path('app/public/backup/'. $user->email));
-            }
+        Mail::send('emails.backup', [], function($message) use ($title, $users) {
+            $message->to('hakimpbg@gmail.com')->subject($title);
+            $message->from(env('MAIL_FROM_ADDRESS'), 'Backup Data');
+        });
+        // foreach($users as $user){
+        //     if(!is_dir(storage_path('app/public/backup/'. $user->email))){
+        //         mkdir(storage_path('app/public/backup/'. $user->email));
+        //     }
 
-            Excel::store(new IncomeExport($user->id), 'public/backup/'. $user->email .'/incomes.xlsx');
-            Excel::store(new PengeluaranExport($user->id), 'public/backup/'. $user->email .'/pengeluaran.xlsx');
+        //     Excel::store(new IncomeExport($user->id), 'public/backup/'. $user->email .'/incomes.xlsx');
+        //     Excel::store(new PengeluaranExport($user->id), 'public/backup/'. $user->email .'/pengeluaran.xlsx');
 
-            Mail::send('emails.backup', [], function($message) use ($title, $user) {
-                $message->to($user->email)->subject($title);
-                $message->from(env('MAIL_FROM_ADDRESS'), 'Backup Data');
-                $message->attach(storage_path('app/public/backup/'. $user->email .'/incomes.xlsx'));
-                $message->attach(storage_path('app/public/backup/'. $user->email .'/pengeluaran.xlsx'));
-            });
-        }
+        //     Mail::send('emails.backup', [], function($message) use ($title, $user) {
+        //         $message->to($user->email)->subject($title);
+        //         $message->from(env('MAIL_FROM_ADDRESS'), 'Backup Data');
+        //         $message->attach(storage_path('app/public/backup/'. $user->email .'/incomes.xlsx'));
+        //         $message->attach(storage_path('app/public/backup/'. $user->email .'/pengeluaran.xlsx'));
+        //     });
+        // }
     }
 }
