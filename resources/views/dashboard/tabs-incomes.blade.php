4c435fcbc8f2ff60830977af6027358d0ad85c8c
Trisa Abdul Hakim <hakimpbg@gmail.com>
2024-01-22 15:23:51 +0700

    update percentage

--- resources/views/dashboard/tabs-incomes.blade.php
+++ resources/views/dashboard/tabs-incomes.blade.php
@@ -4,15 +4,17 @@
         <span
             class="text-2xl font-bold leading-none text-gray-900 sm:text-3xl dark:text-white">{{ $incomeThisMonth }}</span>
         <p class="flex items-center text-base font-normal text-gray-500 dark:text-gray-400">
-            <span class="flex items-center mr-1.5 text-sm text-green-500 dark:text-green-400">
-                <svg class="w-4 h-4" fill="currentColor" viewBox="0 0 20 20"
-                    xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
-                    <path clip-rule="evenodd" fill-rule="evenodd"
-                        d="M10 17a.75.75 0 01-.75-.75V5.612L5.29 9.77a.75.75 0 01-1.08-1.04l5.25-5.5a.75.75 0 011.08 0l5.25 5.5a.75.75 0 11-1.08 1.04l-3.96-4.158V16.25A.75.75 0 0110 17z">
-                    </path>
-                </svg>
-                {{ $percentageIncome }}%
-            </span>
+            @if ($percentageIncome > 0)
+                <span class="flex items-center mr-1.5 text-sm text-green-500 dark:text-green-400">
+                    <i class="fa-solid fa-arrow-up"></i>
+                    {{ $percentageIncome }}%
+                </span>
+            @else
+                <span class="flex items-center mr-1.5 text-sm text-red-500 dark:text-red-400">
+                    <i class="fa-solid fa-arrow-down"></i>
+                    {{ $percentageIncome }}%
+                </span>
+            @endif
             Since last month
         </p>
     </div>
